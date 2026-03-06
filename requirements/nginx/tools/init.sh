0a42e52b1d9448036e20b62e809d45238e437208
KijijiKid <mateo-andre@gmx.de>
2026-03-06 18:27:32 +0100

    Add: https

--- requirements/nginx/tools/init.sh
+++ requirements/nginx/tools/init.sh
@@ -9,11 +9,14 @@ chown -R www:www /www
 
 mv index.html www/
 
+#Set coustom server config
 rm -rf /etc/nginx/nginx.conf
 mv nginx.conf /etc/nginx/
 chmod 777 /etc/nginx/nginx.conf
 
-whoami
+#Create SSL-certificate
+mkdir /etc/nginx/ssl
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/my.key -out /etc/nginx/ssl/my.crt
 
 nginx -t
 
