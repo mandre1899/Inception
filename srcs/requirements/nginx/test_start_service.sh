9e48f324847a8fac53ef00931c6241e8e87a67d8
KijijiKid <mateo-andre@gmx.de>
2026-03-07 10:59:16 +0100

    Add: better container management:

--- srcs/requirements/nginx/test_start_service.sh
+++ srcs/requirements/nginx/test_start_service.sh
@@ -5,7 +5,7 @@ CONTAINERNAME="nginx-Instace"
 
 
 docker build -t "$IMAGENAME" .
-docker run -it --name="$CONTAINERNAME" -p "80:80" -p "443:443" "$IMAGENAME"
+docker run -it --name="$CONTAINERNAME" -p "443:443" "$IMAGENAME"
 
 read -p "Delete instances and image(y/n)?" choice
 if [ "$choice" == "y" ]; then
